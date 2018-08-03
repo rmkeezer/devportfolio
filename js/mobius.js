2618d2edde0c7add3e97a2d900f222b32e65040e
rmkeezer <rmkeezer@yahoo.com>
2018-08-03 01:07:16 -0400

    added temporary link to mobius midi file

--- js/mobius.js
+++ js/mobius.js
@@ -1,17 +1,22 @@
 (function($) {
 
+    console.log("TEST");
     $('#contact-form').submit(function(e){
         e.preventDefault();
+        console.log("SUBMIT");
         spinner = '<i class="fa fa-gear fa-spin" style="font-size:36px; color:white;"></i>';
         $('#results').html(spinner);
         $.ajax({
-            url:'http://98.192.12.55:5000/',
+            url:'//rmkeezer.com:8000/',
             type:'post',
             crossDomain: true,
             data:$('#url-form').serialize(),
             success:function(data){
+                console.log("GOOD");
                 console.log(data);
-                results = '<a class="btn btn-light" href="#">MIDI download</a>';
+                data = JSON.parse(data);
+                console.log(data)
+                results = '<a class="btn btn-light" href="http://98.192.12.55:8000/' + data['filename'] + '.midi">MIDI download</a>';
                 results += '&nbsp;&nbsp;'
                 results += '<a class="btn btn-light" href="#">PDF download</a>';
                 $('#results').html(results);
